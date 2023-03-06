3261572131c4ad17edf3b35f2c4169a80b9ea3e5
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-03-06 21:04:37 +0300

    initialize structure

---
+++ C#/Basics/intruceConcept/intruceConcept/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace intruceConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+            int buradaTanimli = 0;
+            if (true)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    while (true)
+                    {
+                        for (int j = 0; i < 10; i++)
+                        {
+                            if (true)
+                            {
+                                while (true)
+                                {
+                                    int x = 5;
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
