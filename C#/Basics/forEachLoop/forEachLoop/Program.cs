d472647410e0507fbe0a2069c010c8ef9c4fe9e6
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-03-13 19:16:18 +0300

    foreach explained

---
+++ C#/Basics/forEachLoop/forEachLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] products = { "A", "B", "C" };
+
+Array.Resize(ref products, products.Length + 1);
+List<string> productList = new List<string> { "TV", "SoundBar", "XBOX One" };
+
+List<string> shoppingCart = new List<string>();
+
+
+
+
+foreach (var product in productList)
+{
+    shoppingCart.Add(product);
+    productList.Remove(product);
+}
+
