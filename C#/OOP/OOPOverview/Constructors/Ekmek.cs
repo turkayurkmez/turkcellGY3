d2330b13da56f5473c80f623cfe4b739bb7008f3
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-03-14 21:18:53 +0300

    Constructor with Ekmek

--- C#/OOP/OOPOverview/Constructors/Ekmek.cs
+++ C#/OOP/OOPOverview/Constructors/Ekmek.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Constructors
+﻿namespace Constructors
 {
-    internal class Ekmek
+    public class Ekmek
     {
+        public int Adet { get; set; }
+        public string Tur { get; set; }
+
+        public Ekmek() : this("Somun", 1)
+        {
+            //Adet = 1;
+            //Tur = "Somun Ekmek";
+        }
+
+        public Ekmek(int adet) : this("Somun", adet)
+        {
+            //Adet = adet;
+            //Tur = "Somun";
+        }
+
+        public Ekmek(string tur) : this(tur, 1)
+        {
+            //Adet = 1;
+            //Tur = tur;
+        }
+
+        public Ekmek(string tur, int adet)
+        {
+            Tur = tur;
+            Adet = adet;
+        }
+
     }
 }
