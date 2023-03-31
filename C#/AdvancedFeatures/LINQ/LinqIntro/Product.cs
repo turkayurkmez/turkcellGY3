14abbe621a36db54356937583025a6ef450e7009
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-03-31 10:57:18 +0300

    LINQ Completed

--- C#/AdvancedFeatures/LINQ/LinqIntro/Product.cs
+++ C#/AdvancedFeatures/LINQ/LinqIntro/Product.cs
@@ -8,25 +8,40 @@
         public string Description { get; set; }
         public int Stock { get; set; }
 
+        public Category Category { get; set; }
 
 
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 
     public class ProductService
     {
         private List<Product> products;
+        private List<Category> categories;
         public ProductService()
         {
+            categories = new()
+            {
+                new(){ Id=1, Name="Spor"},
+                new(){ Id=2, Name="Giyim"},
+                new(){ Id=3, Name="Kamp"}
+            };
+
             products = new()
             {
-                new(){ Id=1, Name="Bisiklet", Description="Spor amaçlı", Price=3000, Stock=150},
-                new(){ Id=2, Name="Dambıl set", Description="Spor amaçlı", Price=250, Stock=150},
+                new(){ Id=1, Name="Bisiklet", Description="Spor amaçlı", Price=3000, Stock=150, Category=categories[0]},
+                new(){ Id=2, Name="Dambıl set", Description="Spor amaçlı", Price=250, Stock=150, Category = categories[0]},
 
-                new(){ Id=3, Name="Şort", Description="Giyim amaçlı", Price=570, Stock=150},
-                new(){ Id=4, Name="Tişört", Description="Spor amaçlı", Price=150, Stock=50},
-                new(){ Id=5, Name="Çadır", Description="Kampçılık amaçlı", Price=5000, Stock=150},
-                new(){ Id=6, Name="Olta", Description="Keyif amaçlı", Price=2500, Stock=15},
-                new(){ Id=7, Name="Eldiven", Description="Spor amaçlı", Price=500, Stock=150},
+                new(){ Id=3, Name="Şort", Description="Giyim amaçlı", Price=570, Stock=150, Category = categories[1]},
+                new(){ Id=4, Name="Tişört", Description="Spor amaçlı", Price=250, Stock=50, Category = categories[1]},
+                new(){ Id=5, Name="Çadır", Description="Kampçılık amaçlı", Price=5000, Stock=150, Category = categories[2]},
+                new(){ Id=6, Name="Olta", Description="Keyif amaçlı", Price=2500, Stock=15, Category = categories[2]},
+                new(){ Id=7, Name="Eldiven", Description="Spor amaçlı", Price=250, Stock=520, Category=categories[1]},
 
 
             };
@@ -34,6 +49,11 @@
         public List<Product> GetProducts()
         {
             return products;
+        }
+
+        public List<Category> GetCategories()
+        {
+            return categories;
         }
 
     }
--- C#/AdvancedFeatures/LINQ/LinqIntro/Program.cs
+++ C#/AdvancedFeatures/LINQ/LinqIntro/Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LinqIntro;
+using System.Diagnostics;
 
 Console.WriteLine("Hello, World!");
 //LINQ: Language INtegrated Query
 //Bir koleksiyon ile kolayca iş yapabilmek.
-
-var products = new ProductService().GetProducts();
+var service = new ProductService();
+var products = service.GetProducts();
 var x = 5;
 
 var anonymousType = new { Name = "Türkay", Age = 42 };
@@ -17,8 +18,122 @@ var nameAndPriceList = from p in products
 
 nameAndPriceList.ToList().ForEach(x => Console.WriteLine($"......{x.Ad}\t{x.Fiyat}"));
 
+var stopWatch = Stopwatch.StartNew();
+
 var sameResultWithExt = products.Where(p => p.Price < 1000)
                                 .Select(p => new { Ad = p.Name, Fiyat = p.Price })
                                 .ToList();
+stopWatch.Stop();
+Console.WriteLine($"1. sorgunun süresi: {stopWatch.ElapsedMilliseconds.ToString()}");
 
+
+Console.WriteLine("Ext 1");
+sameResultWithExt.ForEach(x => Console.WriteLine($"......{x.Ad}\t{x.Fiyat}"));
+
+stopWatch.Restart();
+
+var same = products.ToList()
+                   .Where(p => p.Price < 1000)
+                   .Select(p => new { Ad = p.Name, Fiyat = p.Price });
+
+stopWatch.Stop();
+Console.WriteLine($"2. sorgunun süresi: {stopWatch.ElapsedMilliseconds.ToString()}");
+
+Console.WriteLine();
+Console.WriteLine("Ext 2");
+same.ToList().ForEach(x => Console.WriteLine($"......{x.Ad}\t{x.Fiyat}"));
+
+Console.WriteLine("-------- FirstOrDefault ---------------");
+var product = products?.FirstOrDefault(p => p.Id == 6);
+
+Console.WriteLine($"6 no'lu ürün {product?.Name}");
+
+//hata verir! Kritere uyan birden fazla satır var:
+//var sportProduct = products.SingleOrDefault(p => p.Description.Contains("Spor"));
+
+var ordered1 = products.OrderBy(p => p.Price)
+                       .ThenByDescending(st => st.Stock)
+
+                       .ToList();
+Console.WriteLine();
+Console.WriteLine("---- Fiyata göre <...>");
+ordered1.ForEach(orderedProduct => Console.WriteLine($"{orderedProduct.Name} {orderedProduct.Price} {orderedProduct.Stock}"));
+
+
+var averagePrice = products.Average(p => p.Price);
+var totalItem = products.Count(p => p.Description.Contains("Spor"));
+Console.WriteLine($"ortalama: {averagePrice}, toplam: {products.Sum(p => p.Price)}, içinde spor geçenlerin sayısı: {totalItem}");
+
+
+//en pahalı ürün:
+var expensiveProduct = products.FirstOrDefault(p => p.Price == products.Max(pr => pr.Price));
+
+Console.WriteLine($"en pahalı ürün: {expensiveProduct.Name} {expensiveProduct.Price} TL");
+
+//JOIN
+/* 
+     Products           
+     Id       Name        CatId                                   
+    ---------------------------------
+     1         A           1
+     2         B           3 
+
+
+     Categories
+     Id       Name 
+     --------------------------------
+     1         X
+     2         Y
+     3         Z 
+
+     Output
+     1        A          X
+     2        B          Z
+     
+
+ */
+
+
+var categories = service.GetCategories();
+var joinQuery = categories.Join(products,
+                                category => category,
+                                product => product.Category,
+                                (category, product) => new { KategoriAdi = category.Name, Urun = product.Name }
+                               )
+                          .ToList();
+
+Console.WriteLine();
+Console.WriteLine("---- Join ----");
+joinQuery.ForEach(res => Console.WriteLine($"{res.Urun} {res.KategoriAdi}"));
+
+Console.WriteLine();
+Console.WriteLine("Alternatif join");
+var join2 = products.Select(pr => new { UrunAdi = pr.Name, Kategorisi = pr.Category?.Name }).ToList();
+
+join2.ForEach(res => Console.WriteLine($"{res.UrunAdi} {res.Kategorisi}"));
+//Group by
+
+var group = products.GroupBy(pr => pr.Category?.Name,
+                             result => new { result.Name, result.Price },
+                             (category, products) => new
+                             {
+                                 Key = category,
+                                 Adet = products.Count(),
+                                 EnUcuz = products.Min(x => x.Price),
+                                 EnPahali = products.Max(x => x.Price)
+                             });
+
+Console.WriteLine();
+Console.WriteLine("-------------------- Group by ------------------");
+
+foreach (var item in group)
+{
+    Console.WriteLine($"Kategori: {item.Key}");
+    Console.WriteLine($"Adet : {item.Adet}");
+    Console.WriteLine($"En ucuz ürün fiyatı: {item.EnUcuz}");
+    Console.WriteLine($"En pahalı ürün fiyatı: {item.EnPahali}");
+
+
+
+}
 
